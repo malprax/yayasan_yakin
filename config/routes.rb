e7762cf0fb0e07afdec5560f083926ace8fd0fa4
malprax <kingmalprax@gmail.com>
2015-05-25 22:41:28 +0800

    add scaffold listings

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  resources :listings
+
   resources :months
 
   resources :years
+  
+  root 'listings#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150525124515) do
+ActiveRecord::Schema.define(version: 20150525140219) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "listings", force: :cascade do |t|
+    t.string   "amount"
+    t.string   "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "months", force: :cascade do |t|
     t.string   "bulan"
