8ecc0f5eecff9afb22600cefa1d47e19290fe470
malprax <kingmalprax@gmail.com>
2015-06-07 16:03:13 +0800

    modify recipient

--- app/models/recipient.rb
+++ app/models/recipient.rb
@@ -1,2 +1,9 @@
 class Recipient < ActiveRecord::Base
+  def alamat_lengkap
+    "#{address}, RT #{rukun_tetangga}/RW #{rukun_warga}, Kelurahan #{kelurahan}, Kecamatan #{kecamatan}, #{province}, #{pos_code}"
+  end
+  
+  def lahir
+    "#{born_place}, #{born_date}"
+  end
 end
