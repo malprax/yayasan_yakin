af17c65b03937d293d701ebd73886be4e1c8e1d5
malprax <kingmalprax@gmail.com>
2015-06-11 22:37:15 +0800

    modify form donaturs

--- app/controllers/cashflows_controller.rb
+++ app/controllers/cashflows_controller.rb
@@ -1,4 +1,5 @@
 class CashflowsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
   before_action :set_cashflow, only: [:show, :edit, :update, :destroy]
 
   # GET /cashflows
