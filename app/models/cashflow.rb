9c0213140b927d8850cc4246ea88a40148531aab
malprax <kingmalprax@gmail.com>
2015-06-11 16:02:59 +0800

    modifiy cashflow

--- app/models/cashflow.rb
+++ app/models/cashflow.rb
@@ -1,2 +1,13 @@
 class Cashflow < ActiveRecord::Base
+  def self.pemasukan
+    self.sum(:debit)
+  end
+
+  def self.pengeluaran
+    self.sum(:credit)
+  end
+  def self.saldo_kas
+    pemasukan - pengeluaran
+  end
 end
+
