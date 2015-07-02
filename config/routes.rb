fbed14e9d1e4add2b415a1566f4af1c4c5d859da
malprax <kingmalprax@gmail.com>
2015-07-02 11:08:55 +0800

    add route and index html

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
   resources :users, only: [:index]
   resources :messages, only: [:new, :create]
+  resources :videos, only: [:index, :new, :create]
   
   get 'home/index'
 
