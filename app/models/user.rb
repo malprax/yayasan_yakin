ffe3a0b0d62acec17efe5f610bf9d04dacdd72b2
malprax <kingmalprax@gmail.com>
2015-07-06 06:24:11 +0800

    add fake authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,9 +23,23 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
          
+  ROLES = {0 => :guest, 1 => :user, 2 => :moderator, 3 => :admin}
+  attr_reader :role
+  
+  def initialize(role_id = 0)
+    @role = ROLES.has_key?(role_id.to_i)? ROLES[role_id.to_i] : ROLES[0]
+    
+  end
+  
+  def role?(role_name)
+    role == role_name
+    
+  end
+         
   acts_as_messageable
   
   def mailboxer_email(object)
     email 
   end
+  
 end
---
+++ spec/models/month.rb
@@ -0,0 +1,5 @@
+require 'spec_helper'
+
+describe Month do
+  it { should validate_presence_of(:set_urut) }
+end
