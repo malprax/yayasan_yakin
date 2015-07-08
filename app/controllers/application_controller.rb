b68f6aa8dc5518043285358fd45ee217116ce44d
malprax <kingmalprax@gmail.com>
2015-07-08 09:42:06 +0800

    add new create project views and controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,13 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  rescue_from CanCan::AccessDenied do |exception|
-    flash[:warning] = exception.message
-    redirect_to root_path
-  end
+  
+  raise CanCan::AccessDenied.new("You are not authorized to perform this action!", :custom_action, Project)
+  
+  # rescue_from CanCan::AccessDenied do |exception|
+#     flash[:warning] = exception.message
+#     redirect_to root_path
+#   end
   
   rescue_from ActiveRecord::RecordNotFound do
     flash[:warning] = 'Resource not found.'
--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -3,4 +3,25 @@ class ProjectsController < ApplicationController
     @projects = Project.all
     authorize! :index, @project
   end
+  
+  def new
+    @project = Project.new
+    authorize! :new, @project
+  end
+  
+  def create
+    @project = Project.new(project_params)
+    if @project.save
+      flash[:success] = 'Project was saved!'
+      redirect_to root_path
+    else
+      render 'new'
+    end
+    authorize! :create, @project
+  end
+  
+  private
+  def project_params
+    params.require(:project).permit(:title)
+  end
 end
